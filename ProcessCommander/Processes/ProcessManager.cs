63950d98ef3aea8cd669d8c9b80e453e068782a2
crabvendor <st.wyrobek@gmail.com>
2019-04-17 16:34:58 +0200

    implement filtering processes that are not accessable

--- ProcessCommander/Processes/ProcessManager.cs
+++ ProcessCommander/Processes/ProcessManager.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
 using ProcessCommander.Models;
 
 namespace ProcessCommander.Processes
 {
-    public class ProcessManager
+    public class MyProcessManager
     {
-        public Process[] GetAllProcesses()
+        public IList<Process> GetAllAccessableProcesses()
         {
-            Process[] processes = Process.GetProcesses();
-            return processes;
+            List<Process> accessedProcesses = new List<Process>();
+            foreach (var process in Process.GetProcesses())
+            {
+                if (IsAccessable(process))
+                {
+                    accessedProcesses.Add(process);
+                }
+            }
+
+            return accessedProcesses;
         }
 
+        private bool IsAccessable(Process process)
+        {
+            try
+            {
+                var processStartTime = process.StartTime;
+            }
+            catch (Win32Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
