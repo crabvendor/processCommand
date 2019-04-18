df58fb79946c326f6396c64d8e3c0a4276d253a4
crabvendor <st.wyrobek@gmail.com>
2019-04-18 12:02:22 +0200

    implement UserResources class which encapsulates metadata about the clients machine

---
+++ ProcessCommander/UserData/UserResources.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProcessCommander.Models;
+
+namespace ProcessCommander.UserData
+{
+    public class UserResources
+    {
+        public string OperatingSystemInfo { get; set; }
+        public string Os { get; set; }
+        public String UserName { get; set; }
+        public string MachineName { get; set; }
+
+        public UserResources()
+        {
+            GatherUserResourcesData();
+        }
+
+        private void GatherUserResourcesData()
+        {
+            this.OperatingSystemInfo = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            this.Os = Environment.OSVersion.ToString();
+            this.UserName = Environment.UserName;
+            this.MachineName = Environment.MachineName;
+        }
+
+        public ResourcesModel GetUserResources()
+        {
+            return new ResourcesModel()
+            {
+                OperatingSystemInfo = this.OperatingSystemInfo,
+                Os = this.Os,
+                UserName = this.UserName,
+                MachineName =  this.MachineName  
+            };
+        }
+    }
+}
