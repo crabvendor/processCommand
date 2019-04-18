daf55d0651babd5df5ecd2bd2acb231dea36bf41
crabvendor <st.wyrobek@gmail.com>
2019-04-18 08:48:42 +0200

    Implement listing meta information about the user and his machine

---
+++ ProcessCommander/Controllers/ResourcesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProcessCommander.Models;
+
+namespace ProcessCommander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourcesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<ResourcesModel> GetResources()
+        {
+            return Ok(new ResourcesModel());
+        }
+    }
+}
---
+++ ProcessCommander/Models/ResourcesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessCommander.Models
+{
+    public class ResourcesModel
+    {
+        public string OperatingSystemInfo => System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+
+        public string Os => Environment.OSVersion.ToString();
+        public string UserName => Environment.UserName;
+
+        public string MachineName => Environment.MachineName;
+        public string ProcessorInfo { get; set; }
+    }
+}
