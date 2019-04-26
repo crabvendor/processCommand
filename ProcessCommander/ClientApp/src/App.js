2f6b2b2d00d13e9e87ee83985d1b404321478843
trauke <piotrekenglert@gmail.com>
2019-04-26 13:15:36 +0200

    add template table

--- ProcessCommander/ClientApp/src/App.js
+++ ProcessCommander/ClientApp/src/App.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, { Component } from "react";
+import { Table } from "reactstrap";
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+class App extends Component {
+  render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>processName</th>
+              <th>startTime</th>
+              <th>totalProcessorTime</th>
+              <th>cpuUsage</th>
+              <th>memoryUsage</th>
+              <th>actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+              <td>
+                <button color="danger" size="sm">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     );
   }
 }
+
+export default App;
